f2730714e3883338a03b6f6236f45a1563d99d1d
LennyAnders <hey@lenny.fyi>
2022-03-14 21:39:12 +0100

    move game functionality into own "module"

--- source/game.lua
+++ source/game.lua
@@ -10,7 +10,69 @@ local sin <const> = math.sin
 
 game = {}
 
+local scoreImage = nil
+local scoreSprite = nil
+
+local circle1 = nil
+local circle2 = nil
+
+local block = nil
 local oldCrankPosition = nil
+
+function updateScoreImage()
+  gfx.pushContext(scoreImage)
+    gfx.clear(gfx.kColorWhite)
+    gfx.drawTextAligned('*Score: ' .. score .. '*', 50, 4, kTextAlignment.center)
+  gfx.popContext()
+end
+
+function increaseScoreBy(number)
+  score += number
+  updateScoreImage()
+end
+
+function game.show()
+  gfx.sprite.removeAll()
+
+  score = 0
+
+  scoreImage = gfx.image.new(100, 25);
+  updateScoreImage()
+  scoreSprite = gfx.sprite.new(scoreImage)
+  scoreSprite:moveTo(350, 12.5)
+  scoreSprite:add()
+
+  local blockImage = gfx.image.new(20, 100, gfx.kColorWhite)
+  block = gfx.sprite.new(blockImage)
+  block:setRotation(45)
+  local width, height = block:getSize()
+  block:setCollideRect(0, 0, width, height)
+  block:moveTo(-width, 70)
+  block:add()
+
+  local circleImage = gfx.image.new(20, 20)
+  gfx.pushContext(circleImage)
+    gfx.fillCircleAtPoint(10, 10, 10)
+  gfx.popContext()
+
+  circle1 = gfx.sprite.new(circleImage)
+  circle2 = gfx.sprite.new(circleImage)
+  game.updateCircles()
+  circle1:setCollideRect(0, 0, circle1:getSize())
+  circle2:setCollideRect(0, 0, circle2:getSize())
+  circle1:add()
+  circle2:add()
+
+  gfx.sprite.setBackgroundDrawingCallback(
+    function ()
+      gfx.drawRect(0, 0, 400, 240)
+
+      gfx.drawCircleAtPoint(330, 120, 51)
+    end
+  )
+
+  gamestate = 'running'
+end
 
 function game.updateCircles()
   local crankPosition = playdate.getCrankPosition()
@@ -22,5 +84,34 @@ function game.updateCircles()
     circle2:moveTo(330 + (cos(circle2Rad) * 50), (120 + sin(circle2Rad) * 50))
 
     oldCrankPosition = crankPosition
+  end
+end
+
+function game.update()
+  game.updateCircles()
+
+  if block.x > 400 then
+    increaseScoreBy(1)
+    block:moveTo(-20, 70)
+  end
+
+  local circleCollisiontest = circle1;
+  local collisions = circleCollisiontest:overlappingSprites()
+  if #collisions < 1 then
+    circleCollisiontest = circle2
+    collisions = circleCollisiontest:overlappingSprites()
+  end
+  local collided = false;
+  if #collisions > 0 then
+    if circleCollisiontest:alphaCollision(collisions[1]) then
+      collided = true
+    end
+  end
+
+  if collided == true then
+    failscreen.show()
+  else
+    block:moveBy(2, 0)
+    gfx.sprite.update()
   end
 end
--- source/main.lua
+++ source/main.lua
@@ -15,26 +15,6 @@ local gfx <const> = playdate.graphics
 gamestate = nil
 score = 0
 
-local scoreImage = nil
-local scoreSprite = nil
-
-circle1 = nil
-circle2 = nil
-
-local block = nil
-
-function updateScoreImage()
-  gfx.pushContext(scoreImage)
-    gfx.clear(gfx.kColorWhite)
-    gfx.drawTextAligned('*Score: ' .. score .. '*', 50, 4, kTextAlignment.center)
-  gfx.popContext()
-end
-
-function increaseScoreBy(number)
-  score += number
-  updateScoreImage()
-end
-
 function init()
   playdate.display.setRefreshRate(50)
   gfx.setColor(gfx.kColorWhite)
@@ -46,55 +26,12 @@ function init()
   startscreen.show()
 end
 
-function startGame()
-  gfx.sprite.removeAll()
-
-  score = 0
-
-  scoreImage = gfx.image.new(100, 25);
-  updateScoreImage()
-  scoreSprite = gfx.sprite.new(scoreImage)
-  scoreSprite:moveTo(350, 12.5)
-  scoreSprite:add()
-
-  local blockImage = gfx.image.new(20, 100, gfx.kColorWhite)
-  block = gfx.sprite.new(blockImage)
-  block:setRotation(45)
-  local width, height = block:getSize()
-  block:setCollideRect(0, 0, width, height)
-  block:moveTo(-width, 70)
-  block:add()
-
-  local circleImage = gfx.image.new(20, 20)
-  gfx.pushContext(circleImage)
-    gfx.fillCircleAtPoint(10, 10, 10)
-  gfx.popContext()
-
-  circle1 = gfx.sprite.new(circleImage)
-  circle2 = gfx.sprite.new(circleImage)
-  game.updateCircles()
-  circle1:setCollideRect(0, 0, circle1:getSize())
-  circle2:setCollideRect(0, 0, circle2:getSize())
-  circle1:add()
-  circle2:add()
-
-  gfx.sprite.setBackgroundDrawingCallback(
-    function ()
-      gfx.drawRect(0, 0, 400, 240)
-
-      gfx.drawCircleAtPoint(330, 120, 51)
-    end
-  )
-
-  gamestate = "running"
-end
-
 init()
 
 function playdate.update()
   if gamestate == "startmenu" then
     if playdate.buttonJustPressed(playdate.kButtonA) then
-      startGame()
+      game.show()
     end
   elseif gamestate == 'highscores' then
     if playdate.buttonJustPressed(playdate.kButtonB) then
@@ -129,32 +66,7 @@ function playdate.update()
     gfx.sprite.update()
     playdate.timer:updateTimers()
   elseif gamestate == 'running' then
-    game.updateCircles()
-
-    if block.x > 400 then
-      increaseScoreBy(1)
-      block:moveTo(-20, 70)
-    end
-
-    local circleCollisiontest = circle1;
-    local collisions = circleCollisiontest:overlappingSprites()
-    if #collisions < 1 then
-      circleCollisiontest = circle2
-      collisions = circleCollisiontest:overlappingSprites()
-    end
-    local collided = false;
-    if #collisions > 0 then
-      if circleCollisiontest:alphaCollision(collisions[1]) then
-        collided = true
-      end
-    end
-
-    if collided == true then
-      failscreen.show()
-    else
-      block:moveBy(2, 0)
-      gfx.sprite.update()
-    end
+    game.update()
   end
 
 	playdate.drawFPS()
